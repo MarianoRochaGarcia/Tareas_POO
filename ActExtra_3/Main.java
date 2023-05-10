5c64b0773a3834a84f5337796ab8cbed664b1122
Rocha03 <marianorocha03@gmail.com>
2023-05-10 12:27:17 -0600

    Commit de Actividad Extra 3

---
+++ ActExtra_3/Main.java
@@ -0,0 +1,8 @@
+package LayoutsJava;
+
+public class Main {
+    public static void main(String[] args) {
+        //Ventana ventana = new Ventana("Layouts");
+        VenatanaGrid venatanaGrid = new VenatanaGrid("Ventana Grid Cuadricula");
+    }
+}
